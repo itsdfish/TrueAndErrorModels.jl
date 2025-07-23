262bf793531e1aa45fe2ccfad548dd6f010c6897
itsdfish <itsdfish@gmail.com>
2025-07-23 04:47:11 -0400

    move utilities to test

--- test/model_tests.jl
+++ test/model_tests.jl
@@ -1,26 +1,26 @@
-# @testitem "tet1_model" begin
-#     using TrueAndErrorModels
-#     using Test
-#     using Turing
-#     using TuringUtilities
+@testitem "tet1_model" begin
+    using TrueAndErrorModels
+    using Test
+    using Turing
+    using TuringUtilities
 
-#     dist = TrueErrorModel(; p = [0.65, 0.15, 0.15, 0.05], ϵ = fill(0.10, 4))
-#     data = rand(dist, 200)
+    n_sim = 200
+    dist = TrueErrorModel(; p = [0.65, 0.15, 0.15, 0.05], ϵ = fill(0.10, 4))
+    data = rand(dist, n_sim)
 
-#     model = tet1_model(data)
-#     chains = sample(model, NUTS(500, 0.65), MCMCThreads(), 500, 4)
+    model = tet1_model(data)
+    chains = sample(model, NUTS(500, 0.65), MCMCThreads(), 500, 4)
 
-#     pred_model = predict_distribution(
-#         TrueErrorModel;
-#         model,
-#         func = x -> x ./ sum(x),
-#         n_samples = 200
-#     )
+    pred_model = predict_distribution(
+        simulator = p -> rand(TrueErrorModel(; p...), n_sim);
+        model,
+        func = x -> x ./ sum(x),
+    )
 
-#     post_preds = generated_quantities(pred_model, chains)
-#     post_preds = stack(post_preds, dims = 1)
-#     @test model() ≠ nothing
-# end
+    post_preds = returned(pred_model, chains)
+    post_preds = stack(post_preds, dims = 1)
+    @test model() ≠ nothing
+end
 
 # @testitem "tet2_model" begin
 #     using TrueAndErrorModels
